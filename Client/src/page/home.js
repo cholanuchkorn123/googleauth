4f9e70ce38ae9518b643e9053b152bb3db117037
cholanuch <cholanuch309651@gmail.com>
2022-12-28 14:19:29 +0700

    add

--- Client/src/page/home.js
+++ Client/src/page/home.js
@@ -2,10 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { gapi } from "gapi-script";
-export function Home({setProfile,profile}) {
+export function Home({ setProfile, profile }) {
   const clientid =
     "723817271422-g0beesp0f0769untnf2aulpchhtfk630.apps.googleusercontent.com";
- 
+  const imgbg = (img) => {
+    if (img == null) {
+      return "https://images.pexels.com/videos/3045163/free-video-3045163.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+    } else return img;
+  };
   useEffect(() => {
     const initClient = () => {
       gapi.client.init({
@@ -35,7 +39,7 @@ export function Home({setProfile,profile}) {
       {(profile.length !== 0 && (
         <div className="flex flex-col gap-[20px] items-center">
           <img
-            src={profile.imageUrl}
+            src={imgbg(profile.imageUrl)}
             alt="user image"
             className="w-[150px] h-[150px] rounded-[50%]"
           />
